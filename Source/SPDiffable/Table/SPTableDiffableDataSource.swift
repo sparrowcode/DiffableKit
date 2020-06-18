b754be829e290b92c3d72ddb08151ba87fda91e9
Ivan Vorobei <hello@ivanvorobei.io>
2020-06-18 15:54:04 +0300

    Fix protect level in controllers

--- Source/SPDiffable/Table/SPTableDiffableDataSource.swift
+++ Source/SPDiffable/Table/SPTableDiffableDataSource.swift
@@ -21,7 +21,7 @@
 
 import UIKit
 
-public class SPTableDiffableDataSource: UITableViewDiffableDataSource<SPDiffableSection, SPDiffableItem> {
+open class SPTableDiffableDataSource: UITableViewDiffableDataSource<SPDiffableSection, SPDiffableItem> {
     
     public init(tableView: UITableView, cellProviders: [CellProvider]) {
         super.init(tableView: tableView, cellProvider: { (tableView, indexPath, model) -> UITableViewCell? in
