333b7aad1602b5aa01dffc96d28a7b9f94a95d60
Ivan Vorobei <hello@ivanvorobei.io>
2020-07-10 00:35:20 +0300

    Fix example

--- Example/Controllers/DiffableTableController.swift
+++ Example/Controllers/DiffableTableController.swift
@@ -2,8 +2,12 @@ import UIKit
 
 class DiffableTableController: SPDiffableTableController, SPTableDiffableMediator {
     
-    override init() {
+    init() {
         super.init(style: .insetGrouped)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
