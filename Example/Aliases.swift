72751342616a52ac60d58120c8e5f62250a1a343
Ivan Vorobei <varabeis@icloud.com>
2020-08-20 12:59:03 +0300

    Add aliases

---
+++ Example/Aliases.swift
@@ -0,0 +1,33 @@
+// import SPDiffable
+import UIKit
+
+/**
+ This file using as wrapper for ignore import SPDiffable libary.
+ */
+
+typealias DiffableTableDataSource = SPDiffableTableDataSource
+typealias DiffableTableMediator = SPDiffableTableMediator
+typealias DiffableTableViewCell = SPDiffableTableViewCell
+typealias DiffableSubtitleTableViewCell = SPDiffableSubtitleTableViewCell
+
+// MARK:  Basic models
+
+typealias DiffableItem = SPDiffableItem
+typealias DiffableSection = SPDiffableSection
+
+// MARK:  Table models
+
+typealias TableRow = SPDiffableTableRow
+typealias TableRowSubtitle = SPDiffableTableRowSubtitle
+typealias TableRowSwitch = SPDiffableTableRowSwitch
+typealias TableRowStepper = SPDiffableTableRowStepper
+typealias TableRowButton = SPDiffableTableRowButton
+
+typealias DiffableTextHeaderFooter = SPDiffableTextHeaderFooter
+typealias DiffableTableCellProvider = SPDiffableTableCellProvider
+
+// MARK: Other
+
+typealias DiffableTableStepper = SPDiffableStepper
+typealias DiffableTableSwitch = SPDiffableSwitch
+
