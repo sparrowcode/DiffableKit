d1ce95beb1dcdb36a0a303c0a56c0edd09490094
Ivan Vorobei <hello@ivanvorobei.io>
2021-05-05 09:07:57 +0300

    Update packages.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.4
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -11,7 +10,8 @@ let package = Package(
     products: [
         .library(
             name: "SPDiffable",
-            targets: ["SPDiffable"]),
+            targets: ["SPDiffable"]
+        ),
     ],
     targets: [
         .target(
