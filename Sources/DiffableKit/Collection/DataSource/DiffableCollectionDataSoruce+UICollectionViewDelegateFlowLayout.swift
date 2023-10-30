3fb63a1072e5688d0384e6be4738eda2690cd01a
Ivan Vorobei <hello@ivanvorobei.io>
2023-10-31 00:45:57 +0300

    Addded sections insets.

--- Sources/DiffableKit/Collection/DataSource/DiffableCollectionDataSoruce+UICollectionViewDelegateFlowLayout.swift
+++ Sources/DiffableKit/Collection/DataSource/DiffableCollectionDataSoruce+UICollectionViewDelegateFlowLayout.swift
@@ -5,4 +5,8 @@ extension DiffableCollectionDataSource: UICollectionViewDelegateFlowLayout {
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return diffableLayoutDelegate?.diffableCollectionView(collectionView, layout: collectionViewLayout, sizeForItemAt: indexPath) ?? .zero
     }
+    
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return diffableLayoutDelegate?.diffableCollectionView(collectionView, layout: collectionViewLayout, insetForSectionAt: section) ?? .zero
+    }
 }
--- Sources/DiffableKit/Collection/DiffableCollectionViewDelegateFlowLayout.swift
+++ Sources/DiffableKit/Collection/DiffableCollectionViewDelegateFlowLayout.swift
@@ -3,9 +3,13 @@ import UIKit
 public protocol DiffableCollectionViewDelegateFlowLayout: AnyObject {
  
     func diffableCollectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    
+    func diffableCollectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets
 }
 
 public extension DiffableCollectionViewDelegateFlowLayout {
     
     func diffableCollectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize { return .zero }
+    
+    func diffableCollectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets { return .zero }
 }
